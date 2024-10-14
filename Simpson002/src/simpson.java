8a563ec92d1afe9ff5dc931bc9b32ee37a2ea08e
maarcox <marcospaniego@gmail.com>
2024-10-14 10:48:52 +0200

    simpson002
    simpson002

--- Simpson002/src/simpson.java
+++ Simpson002/src/simpson.java
@@ -1,12 +1,14 @@
 import java.util.Random;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class simpson {
     private static final int MAX_FILA_TABLERO = 10;
     private static final int MAX_COLUMNA_TABLERO = 10;
-
     private static char[][] tablero;
+    private static int filaBart;
+    private static int columnaBart;
 
     public static void imprimirtablero(){
         // Imprimir tablero
@@ -28,8 +30,8 @@ public class simpson {
 
     private static void asignarPersonajesencasillaLibre(char caracter, int numrepeticiones){
         Random aleatorio = new Random();
-        int FilaAleatorio;
-        int columnaAleatorio;
+        int FilaAleatorio =-1;
+        int columnaAleatorio =-1;
         for (int i = 0; i < numrepeticiones; i++) {
             do {
                 FilaAleatorio = aleatorio.nextInt(MAX_FILA_TABLERO);
@@ -37,8 +39,11 @@ public class simpson {
             }while (tablero[FilaAleatorio][columnaAleatorio]!='-');
             tablero[FilaAleatorio][columnaAleatorio] = caracter;
         }
-            
+        if (caracter == 'B') {
+            filaBart = FilaAleatorio;
+            columnaBart = columnaAleatorio;
         }
+    }
     public static void main(String[] args) {
         //1º) Inicializo mi matriz tablero
         tablero = new char[MAX_FILA_TABLERO][MAX_COLUMNA_TABLERO];
@@ -61,8 +66,57 @@ public class simpson {
         //7ºImprimirtablero;
         imprimirtablero();
 
-       
+        /* DESPLAZAMIENTO DE BART */
+            /*A----izquierda*/
+            /*D----derecha*/
+            /*W----arriba*/
+            /*S----abajo*/
+        Scanner lector = new Scanner(System.in);
+        /* ***************************************************************************** */
+        /*                                   JUGAR                                       */
+        /* ***************************************************************************** */
+        int vidas  = 10;
+       do {
+           System.out.println("Diga es desplazamiento que deseas realizar");
+           System.out.println("A---> Izquierda, D---> Derecha, W---> Arriba, S---> Abajo");
+           String desplazamiento = lector.nextLine();
+           System.out.println("Desplazamiento=" + desplazamiento);
 
+           switch (desplazamiento){
+               case "A": //izquierda
+                   if ((columnaBart - 1 )>=0 ){
+                       columnaBart = columnaBart -1;
+                       switch (tablero[filaBart][columnaBart]){
+                           case 'H':
+                               vidas = vidas-1;
+                               System.out.println("Te quedan "+vidas+ " vidas");
+                               tablero[filaBart][columnaBart] = 'B';
+                               tablero[filaBart][columnaBart+1] = '-';
+                               break;
+                           case 'M':
+                                System.out.println("El muro no te deja desplazarte a esta casilla");
+                                break;
+
+                       }
+                       tablero[filaBart][columnaBart] = 'B';
+                       tablero[filaBart][columnaBart+1] = '-';
+                   }else{
+                       System.out.println("Desplazamiento prohibido. Límite de tablero.");
+                   }
+
+                   break;
+               case "D":
+                   break;
+               case "W":
+                   break;
+               case "S":
+                   break;
+               default:
+                   break;
+           }
+
+           imprimirtablero();
+       }while (vidas>0);
         //FIN
     }
 }
