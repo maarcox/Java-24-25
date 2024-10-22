83c82d86197b7455363af231297f0035589a2d29
maarcox <marcospaniego@gmail.com>
2024-10-22 13:56:57 +0200

    EjercicioOptimizarCodigo
    EjercicioOptimizarCodigo

---
+++ EjercicioOptimizarCodigo001/src/Main.java
@@ -0,0 +1,34 @@
+//En este ejercicio nos piden que Optimizemos el codigo que nos mandaron en el enunciado
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+if (block == "IRON") {
+if (block == "BED") {
+if (block == "BED_BLOCK") {
+if (block == "CACTUS") {
+if (block == "BRICK_STAIRS) {
+if (block =="BIRCH_WOOD_STAIRS) {
+if (block == "BREWING_STAND) {
+if (block == "CAKE) {
+if (block == "CAKE_BLOCK) {
+if (block == "CAULDRON) {
+if (block == "CHEST) {
+if (block == "CLAY) {
+if (block == "SAND) {
+if (block == "BURNING_FURNACE) {
+if (block == "COBBLESTONE_STAIRS) {
+if (block == "FENCE) {
+if (block == "FENCE_GATE) {
+if (block == "FIRE) {
+if (block == "GRAVEL) {
+if (block == "IRON_DOOR_BLOCK) {
+if (block == "IRON_FENCE) {
+if (block == "IRON_DOOR) {
+if (block == "LADDER) {
+if (block == "MELON) {
+if (block == "LEAVES) {
+if (block == "LOCKED_CHEST) {
+if (block == "SANDSTONE_STAIRS) {
