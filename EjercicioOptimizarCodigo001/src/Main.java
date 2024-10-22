e54d8c89a84ba1174f79a2a91a9b9847258f0d26
marcosypaulaxv <marcospaniego@gmail.com>
2024-10-22 18:54:45 +0200

    EjercicioOptimizarCodigo001
    EjercicioOptimizarCodigo001

--- EjercicioOptimizarCodigo001/src/Main.java
+++ EjercicioOptimizarCodigo001/src/Main.java
@@ -1,34 +1,54 @@
 //En este ejercicio nos piden que Optimizemos el codigo que nos mandaron en el enunciado
 
 public class Main {
+    private static String[] palabras;
+    private static void ListaDePalabras(){
+
+        palabras = new String[]{
+                "IRON",
+                "BED",
+                "BED_BLOCK",
+                "CACTUS",
+                "BRICK_STAIRS",
+                "BIRCH_WOOD_STAIRS",
+                "BREWING_STAND",
+                "CAKE",
+                "CAKE_BLOCK",
+                "CAULDRON",
+                "CHEST",
+                "CLAY",
+                "SAND",
+                "BURNING_FURNACE",
+                "COBBLESTONE_STAIRS",
+                "FENCE",
+                "FENCE_GATE",
+                "FIRE",
+                "GRAVEL",
+                "IRON_DOOR_BLOCK",
+                "IRON_FENCE",
+                "IRON_DOOR",
+                "LADDER",
+                "MELON",
+                "LEAVES",
+                "LOCKED_CHEST",
+                "SANDSTONE_STAIRS"
+        } ;
+    }
+
     public static void main(String[] args) {
+        ListaDePalabras();
+        String block = "MELON";
+        boolean TrueoFalse = true;
+        for(int i = 0; i < palabras.length; i++){
+            if (palabras[i] == block){
+                TrueoFalse = false;
+            }
+        }
+        if(TrueoFalse == false){
+            System.out.println("La palabra "+ block +" esta dentro de la lista");
+        }else{
+            System.out.println("La palabra "+ block +" no esta en la lista");
+        }
 
     }
 }
-if (block == "IRON") {
-if (block == "BED") {
-if (block == "BED_BLOCK") {
-if (block == "CACTUS") {
-if (block == "BRICK_STAIRS) {
-if (block =="BIRCH_WOOD_STAIRS) {
-if (block == "BREWING_STAND) {
-if (block == "CAKE) {
-if (block == "CAKE_BLOCK) {
-if (block == "CAULDRON) {
-if (block == "CHEST) {
-if (block == "CLAY) {
-if (block == "SAND) {
-if (block == "BURNING_FURNACE) {
-if (block == "COBBLESTONE_STAIRS) {
-if (block == "FENCE) {
-if (block == "FENCE_GATE) {
-if (block == "FIRE) {
-if (block == "GRAVEL) {
-if (block == "IRON_DOOR_BLOCK) {
-if (block == "IRON_FENCE) {
-if (block == "IRON_DOOR) {
-if (block == "LADDER) {
-if (block == "MELON) {
-if (block == "LEAVES) {
-if (block == "LOCKED_CHEST) {
-if (block == "SANDSTONE_STAIRS) {
