9d6c5b1ac9388cc6a61343acbc762324fa1058fc
marcosypaulaxv <marcospaniego@gmail.com>
2024-10-28 18:28:02 +0100

    ProgamameAlturaPiramides
    ProgamameAlturaPiramides

--- Progamame-AlturaPiramide/src/AlturaPiramides.java
+++ Progamame-AlturaPiramide/src/AlturaPiramides.java
@@ -1,14 +1,5 @@
 import java.util.Scanner;
-
 public class AlturaPiramides {
-
-    //Nivel 1 mas alto piramide = 1 bloque
-    //nivel 2 Penultimo 3x3 = 9 bloques
-    //nivel 3 Antepenultimo 5x5 = 25 bloques
-    //nivel 4 7x7 = 49 bloques
-    //...
-
-
     private static int maxNiveles(long bloques) {
         int n = 0;
         long totalBloques = 0;
@@ -25,14 +16,21 @@ public class AlturaPiramides {
     public static void main(String[] args) {
         System.out.println("Altura De La Piramide");
         Scanner scanner = new Scanner(System.in); //Creamos un scanner
+
         while (true) {
 
-            long bloques = scanner.nextLong(); //falta poner limite al usuario numero de bloques comprendido entre 0 y 2.000.000.000
+            long bloques = scanner.nextLong();
 
             if (bloques == 0) {
                 break; // Termina la entrada al recibir un 0
             }
-            System.out.println(maxNiveles(bloques));
+            if (bloques < 0 || bloques >2000000000) {
+                System.out.println("Numero invalido");
+                break;
+            }
+            if (bloques > 0 || bloques < 2000000000) {
+                System.out.println(maxNiveles(bloques));
+            }
         }
 
         scanner.close();
