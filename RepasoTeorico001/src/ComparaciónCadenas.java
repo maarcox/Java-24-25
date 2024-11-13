7ee5c916a92df6d67038aaaa0409d56c9fb12fb9
maarcox <marcospaniego@gmail.com>
2024-11-13 10:10:52 +0100

    Comparación Cadenas
    Comparación Cadenas

---
+++ RepasoTeorico001/src/ComparaciónCadenas.java
@@ -0,0 +1,13 @@
+public class ComparaciónCadenas {
+    public static void main(String[] args) {
+        String cadena1 = "Hola";
+        String cadena2 = "Hola";
+
+        if (cadena1 == cadena2) {
+            System.out.println("Las cadenas son iguales");
+        } else {
+            System.out.println("Las cadenas son diferentes");
+        }
+    }
+
+}
