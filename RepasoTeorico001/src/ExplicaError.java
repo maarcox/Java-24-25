4077fbf9bc557a01cca7f7a0a0cc83e2efbebccc
maarcox <marcospaniego@gmail.com>
2024-11-13 10:24:49 +0100

    Explica Error
    Explica Error

---
+++ RepasoTeorico001/src/ExplicaError.java
@@ -0,0 +1,13 @@
+public class ExplicaError {
+    public static void main(String[] args) {
+        int[][] matriz = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        System.out.println("Elemento en la posición (2,2): " + matriz[2][2]);
+        //Este error se debe a que estas intentando acceder
+        //a un indice que no existe dentro de la matriz, que
+        // esta fuera de rangos
+    }
+}
